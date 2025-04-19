50b8c714642f0d6c07d39b55c844a266e16359c5
isaias <isaiasgarraeluta@gmail.com>
2025-04-19 17:38:38 -0300

    fix

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,7 +3,6 @@ import { DefaultCommander } from "gear-roboto";
 import path from "path";
 
 
-
 const commander = new DefaultCommander(["/","#","."])
 
 commander.addCommandsByPath(path.resolve("src","commands"))
